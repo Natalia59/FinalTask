078e3e61e452a65b9c67e9723403db578f42201c
Natalia59 <maxi77@bk.ru>
2022-11-24 12:49:58 +0200

    Program

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ void SecondArrayWithIF(string[] array1, string[] array2)
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-    if(array1[i].Length <= 3)
+        if(array1[i].Length <= 3)
         {
-        array2[count] = array1[i];
-        count++;
+           array2[count] = array1[i];
+           count++;
         }
     }
 }
